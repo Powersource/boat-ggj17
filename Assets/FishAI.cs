e88205629de27529144979f3b1a4379bf02bd89b
LJoley <lukasjoley@gmail.com>
2017-01-21 15:35:19 +0100

    Even better AI

--- Assets/FishAI.cs
+++ Assets/FishAI.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class FishAI : MonoBehaviour {
 
     public float mySwimSpeed = 1f;
+
+    public float frequency = 11.1f;
+    public float magnitude = 0.5f;
+    private int offset = 1;
+    private Vector3 axis;
+
+
     private GameObject myGameLogic;
     private float myKillX;
     private int myScoreValue;
@@ -12,9 +19,13 @@ public class FishAI : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         //make dynamic based on camera bounds!
+
+
         myKillX = -14;
         myGameLogic = GameObject.FindGameObjectWithTag("GameLogic").gameObject;
         myScoreValue = Random.Range(1, 11);
+
+        offset = Random.Range(1, 5);
         
         switch(myScoreValue)
         {
@@ -24,6 +35,7 @@ public class FishAI : MonoBehaviour {
 
                 gameObject.GetComponent<SpriteRenderer>().color = Color.green;
                 myScoreValue = 10;
+                magnitude = 0.3f;
                 break;
 
             case 4:
@@ -32,6 +44,7 @@ public class FishAI : MonoBehaviour {
 
                 gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
                 myScoreValue = 15;
+                magnitude = 0.5f;
                 break;
 
             case 7:
@@ -39,6 +52,7 @@ public class FishAI : MonoBehaviour {
 
                 gameObject.GetComponent<SpriteRenderer>().color = Color.cyan;
                 myScoreValue = 20;
+                magnitude = 0.8f;
                 break;
 
             
@@ -46,21 +60,24 @@ public class FishAI : MonoBehaviour {
 
                 gameObject.GetComponent<SpriteRenderer>().color = Color.red;
                 myScoreValue = 30;
+                magnitude = 1;
                 break;
 
             case 10:
 
                 gameObject.GetComponent<SpriteRenderer>().color = Color.yellow;
                 myScoreValue = 50;
+                magnitude = 1.1f;
                 break;
         }
         
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        transform.Translate(-mySwimSpeed, 0, 0);
+	void FixedUpdate () {
 
+        transform.position = new Vector2(transform.position.x - mySwimSpeed, (Mathf.Sin(transform.position.x* magnitude)));
+        
         if(transform.position.x < myKillX)
         {
             Debug.Log("Fish destroying itself...");
@@ -73,7 +90,6 @@ public class FishAI : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D other)
     {
         myGameLogic.SendMessageUpwards("UpdateScore", myScoreValue);
-        //Debug.Log(myScoreValue);
         Destroy(gameObject);
     }
 }
